a4ea70c11bba45e793210b14645d5d68fca864e4
mwas <francismwangi152@gmail.com>
2015-08-04 00:19:58 +0300

    changed application name

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker push mwaaas/django-sample-app:$SHA1
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name hello \
+aws elasticbeanstalk create-application-version --application-name django-docker-sample \
     --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
