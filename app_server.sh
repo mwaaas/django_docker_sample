ecfef4113e0f397eb6adbf0dcd10e60fea03e72e
mwas <francismwangi152@gmail.com>
2015-08-09 11:26:28 +0300

    added all types of logs

--- app_server.sh
+++ app_server.sh
@@ -4,4 +4,7 @@ exec  gunicorn --bind=0.0.0.0:80 django_docker_sample.wsgi \
         --settings=django_docker_sample.settings\
         --log-level=info \
         --log-syslog \
-        --log-syslog-to=udp://cloudwatchlogs:514
+        --log-syslog-to=udp://cloudwatchlogs:514 \
+        --log-file=-\
+        --access-logfile=-\
+        --error-logfile=-
